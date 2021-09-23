d0808250e2b4229f9d3b931316a6d15d559001ae
njmariaqi <88069097+njmariaqi@users.noreply.github.com>
2021-09-23 16:36:03 -0500

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ function redirectRecipe() {
 document.addEventListener("keyup", function(event) {          
      if (event.key === "Enter") {
           event.preventDefault();
-          window.location.href = '../';
+          window.location.href = `Stay-Fit/html/recipe.html?searchInput=${searchInput.value}`;
      }
 }) }
 
