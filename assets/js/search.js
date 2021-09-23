10f20050b77fd17f27e64f832dd9112d6f5b54ad
njmariaqi <88069097+njmariaqi@users.noreply.github.com>
2021-09-23 16:27:34 -0500

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ function redirectRecipe() {
 document.addEventListener("keyup", function(event) {          
      if (event.key === "Enter") {
           event.preventDefault();
-          window.location.href = `/html/recipe.html?searchInput=${searchInput.value}`;
+          window.location.href = '/test';
      }
 }) }
 
